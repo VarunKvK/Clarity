706226f6df365727b847b3862410d8812cb36e6d
Varun <varunkrishnankv2002@gmail.com>
2024-10-27 13:43:00 +0530

    Signing option modified on homepage

--- frontend/src/components/mini_components/Upload.jsx
+++ frontend/src/components/mini_components/Upload.jsx
@@ -3,27 +3,30 @@ import React, { useState } from "react";
 import { FileUpload } from "@/components/ui/file-upload";
 import { useToast } from '@/hooks/use-toast'
 import { CheckCircle } from "lucide-react";
+import { SignInButton, useAuth } from "@clerk/nextjs";
 
 
 export function FileUploader({ uploaded, setFiles }) {
   const { toast } = useToast()
+  const {isSignedIn}=useAuth();
+
 
   const handleFileUpload = async (f) => {
     setFiles(f);
     try {
       if (f[0].type === "application/pdf") {
         const formData = new FormData();
-        formData.append("file", f[0]); 
-  
-        const response = await fetch("http://127.0.0.1:8000/newuploads/", { 
+        formData.append("file", f[0]);
+
+        const response = await fetch("http://127.0.0.1:8000/newuploads/", {
           method: "POST",
           body: formData,
         });
-  
+
         if (response.ok) {
           const data = await response.json();
           console.log(data);
-          uploaded(true); 
+          uploaded(true);
           toast(
             {
               icon: <CheckCircle size={24} />,
@@ -57,12 +60,20 @@ export function FileUploader({ uploaded, setFiles }) {
       );
     }
   };
-  
+
 
   return (
     <div
-      className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-black border-[#cf0] rounded-lg">
-      <FileUpload onChange={handleFileUpload} />
+      className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-black border-[#cf0] rounded-lg flex justify-center items-center">
+      {isSignedIn ? (<FileUpload onChange={handleFileUpload} />):(
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col items-center justify-center">
+          <h2 className="text-lg">Haven't signed in yet?</h2>
+          <p className="opacity-20">Let's complete that step first!</p>
+          </div>
+        <SignInButton className="bg-[#cf0] rounded-md px-4 py-1.5 text-[#111]"/>
+        </div>
+        )}
     </div>
   );
 }
