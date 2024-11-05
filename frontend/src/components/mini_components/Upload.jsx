f6cf671763f06fdfef4d9c91a34a7a1ecbcd2499
Varun <varunkrishnankv2002@gmail.com>
2024-11-05 14:50:33 +0530

    Backend Deployment on the Render Server-II

--- frontend/src/components/mini_components/Upload.jsx
+++ frontend/src/components/mini_components/Upload.jsx
@@ -9,6 +9,7 @@ export function FileUploader({ files, setFiles, setuploaded }) {
   const { toast } = useToast();
   const { isSignedIn } = useAuth();
 
+  console.log(`${process.env.NEXT_PUBLIC_API_URL}`)
   const handleFileUpload = async (f) => {
     setFiles(f);
     setuploaded(true);
